fa03f19d0ce033ff92b4469b8ef9f72c6f7e95bc
Diamondooooo <almaz.sgv1@mail.ru>
2023-06-18 19:32:03 +0500

    React and css webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,10 +1,22 @@
 import webpack from 'webpack';
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      'style-loader',
+      // Translates CSS into CommonJS
+      'css-loader',
+      // Compiles Sass to CSS
+      'sass-loader',
+    ],
+  };
+  // Если не используем тайпскрипт - нужен babel-loader(берет новый стандарт js и перегоняет его в старый, чтобы все браузеры поддерживались)
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   };
-  return [typescriptLoader];
+  return [typescriptLoader, cssLoader];
 }
