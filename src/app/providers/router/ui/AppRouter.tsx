8fbab3b7b1cae5f2071069e978c07c25d7b9280a
Diamondooooo <almaz.sgv1@mail.ru>
2023-06-26 14:30:00 +0500

    реализовал Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -4,13 +4,17 @@ import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}> 
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         {Object.values(routeConfig).map(({ element, path }) => (
           <Route
             key={path}
             path={path}
-            element={<Suspense fallback={<div>Loading...</div>}>{element}</Suspense>}
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <div className='page-wrapper'>{element}</div>
+              </Suspense>
+            }
           />
         ))}
       </Routes>
