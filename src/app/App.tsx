7731574964cff6837062514431b2359372168852
Diamondooooo <almaz.sgv1@mail.ru>
2023-06-27 15:19:00 +0500

    add i18n and Defin plugin

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,23 @@
-import { Link } from 'react-router-dom';
-import { useTheme } from './providers/ThemeProvider';
-import { AppRouter } from './providers/router';
-import './styles/index.scss';
+import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { useTheme } from './providers/ThemeProvider';
+import { AppRouter } from './providers/router';
+import './styles/index.scss';
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar />
-      <div className='content-page'>
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=''>
+        <Navbar />
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
