f8d41ef5f17859af29eda1471aff017796626fea
Diamondooooo <almaz.sgv1@mail.ru>
2023-07-06 15:52:40 +0500

    add error boundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,9 @@ import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import './styles/index.scss';
 import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
+import './styles/index.scss';
 
 function App() {
     const { theme } = useTheme();
