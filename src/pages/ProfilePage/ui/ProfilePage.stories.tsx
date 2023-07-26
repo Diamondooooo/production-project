a7d9345a14f9522c5f6feb010e8147717c80b45a
Diamondooooo <almaz.sgv1@mail.ru>
2023-07-26 18:15:34 +0500

    fix common.ts

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -1,8 +1,8 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import ProfilePage from './ProfilePage';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import ProfilePage from './ProfilePage';
 
 export default {
     title: 'pages/ProfilePage',
